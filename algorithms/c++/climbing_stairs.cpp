a44d040ecea58c3a39800175862cc45c86007a0f
annie95162 <91278852+annie95162@users.noreply.github.com>
2025-07-15 11:57:50 +0800

    Update climbing_stairs.cpp

--- algorithms/c++/climbing_stairs.cpp
+++ algorithms/c++/climbing_stairs.cpp
@@ -1,1 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int p = 1, q = 1, r = 1;
+        for(int i = 2; i <= n; i++){
+            r = q;
+            q = p + q;
+            p = r;
+        }
+        return q;
 
+    }
+};
