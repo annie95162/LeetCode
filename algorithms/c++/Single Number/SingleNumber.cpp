36d1ff1dfc5dc0a22b75000837ed70ba84ef9b5b
annie95162 <91278852+annie95162@users.noreply.github.com>
2025-07-29 13:46:37 +0800

    Update SingleNumber.cpp

--- algorithms/c++/Single Number/SingleNumber.cpp
+++ algorithms/c++/Single Number/SingleNumber.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin(),nums.end());
+        if(nums.size() == 1){
+            return nums[0];
+        }
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        if(nums[nums.size()-1] != nums[nums.size()-2]){
+            return nums[nums.size()-1];
+        }
+        for(int i = 1; i < nums.size()-1; i++){
+            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]){
+                ans = nums[i];
+                break;
+            }
+        } 
+        return ans;
+    }
+};
