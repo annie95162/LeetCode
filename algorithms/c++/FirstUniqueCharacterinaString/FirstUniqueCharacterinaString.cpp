ea5c1ef2b6a2925d17a763ab533ee009d43b7725
annie95162 <91278852+annie95162@users.noreply.github.com>
2025-07-31 14:31:55 +0800

    Update FirstUniqueCharacterinaString.cpp

--- algorithms/c++/FirstUniqueCharacterinaString/FirstUniqueCharacterinaString.cpp
+++ algorithms/c++/FirstUniqueCharacterinaString/FirstUniqueCharacterinaString.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int ans = -1;
+        unordered_map<char,vector<int>> map;
+        for(int i = 0; i < s.size(); i++){
+            map[s[i]].emplace_back(i);
+        }
+        for(int j = 0; j < s.size(); j++){
+            if(map[s[j]].size() == 1){
+                ans = map[s[j]][0];
+                break;
+            }
+        }
+        return ans;
+    }
+};
