df30cd081dcc7ad2d62ae1bcf2c73ce38293ac41
annie95162 <91278852+annie95162@users.noreply.github.com>
2025-08-06 15:48:47 +0800

    Update ImplementStackusingQueues.cpp

--- algorithms/c++/Implement Stack using Queues/ImplementStackusingQueues.cpp
+++ algorithms/c++/Implement Stack using Queues/ImplementStackusingQueues.cpp
@@ -1,3 +1,7 @@
+// Source : https://leetcode.com/problems/implement-stack-using-queues/
+// Author : Min Fang
+// Date   : 2025-08-06
+
 /**********************************************************************************
  *
  * Problem:
@@ -61,4 +65,48 @@
  * - Guarantees correct LIFO behavior by rotating elements after each push.
  *
  **********************************************************************************/
+class MyStack {
+private:
+    queue<int> q1;
+    queue<int> q2;
+    void reverse(int t){
+        q2.push(t);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+        
+    }
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        reverse(x);
+    }
+    
+    int pop() {
+        int p = q1.front();
+        q1.pop();
+        return p;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
 
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
