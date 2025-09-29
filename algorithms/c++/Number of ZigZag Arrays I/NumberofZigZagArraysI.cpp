8685a950cfe3d04ef93fd79a90b62a4b36ba2909
annie95162 <91278852+annie95162@users.noreply.github.com>
2025-09-29 20:58:36 +0800

    Update NumberofZigZagArraysI.cpp

--- algorithms/c++/Number of ZigZag Arrays I/NumberofZigZagArraysI.cpp
+++ algorithms/c++/Number of ZigZag Arrays I/NumberofZigZagArraysI.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int zigZagArrays(int n, int l, int r) {
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(r+1,0)));
+        for(int i = l; i <= r; i++){
+            dp[1][0][i] = 1;
+            dp[1][1][i] = 1;
+        }
+        vector<vector<int>> pf(r+1, vector<vector<int>>(2,0));
+        for(int )
+        for(int i = 2; i <= n; i++){
+            
+        }
+    }
+};
